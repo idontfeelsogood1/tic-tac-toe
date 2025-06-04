f10889b46d1ffd15afa21e5b6024309fdacc5720
Le Duong <leduongnewmemes123@gmail.com>
2025-06-05 02:03:23 +0700

    Finished project

--- script.js
+++ script.js
@@ -7,45 +7,103 @@ const gameBoard = (function () {
     return { arr };
 })();
 
+
+const whenPlayButtonClicked = (function () {
+    document.querySelector(".play").addEventListener("click", function prepareGame() {
+        const boards = document.querySelectorAll("#game-board > button");
+        const players = document.querySelectorAll(".player");
+
+        for (let board of boards) {
+            board.removeAttribute("disabled");
+            board.className = "board";
+        }
+        
+        for (let player of players ) {
+            player.setAttribute("disabled", "disabled");
+        }
+    })
+})();
+
+
 const displayController = (function () {
     let player1 = createPlayer('idontfeelsogood1', 'x');
     let player2 = createPlayer('idontfeelsogood2', 'o');
-    
+    let currentPlayer = player1.name;
+    let boardCounter = 0;
+    let hasWon = false;
+    let winner = document.querySelector(".winner");
+
+    const boards = document.querySelectorAll("#game-board > button");
+    for (let board of boards) {
+        board.addEventListener("click", () => {
+            if (currentPlayer === player1.name && board.innerText === '' && hasWon != true) {
+                gameBoard.arr[Number(board.dataset.x)][Number(board.dataset.y)] = player1.mark;
+                board.innerText = player1.mark;
+                if (checkWin(player1.mark) === 'Win') {
+                    console.log('Player1');
+                    hasWon = true;
+                    winner.innerText = `WINNER: ${player1.name}`;
+                } 
+                currentPlayer = player2.name;
+                boardCounter++;
+            } else if (currentPlayer === player2.name && board.innerText === '' && hasWon != true) {
+                gameBoard.arr[Number(board.dataset.x)][Number(board.dataset.y)] = player2.mark;
+                board.innerText = player2.mark;
+                if (checkWin(player2.mark) === 'Win') {
+                    console.log('Player2');
+                    hasWon = true;
+                    winner.innerText = `WINNER: ${player2.name}`;
+                }
+                currentPlayer = player1.name;
+                boardCounter++;
+            }
+
+            if (hasWon === true || boardCounter === 9 && hasWon != true) {
+                playBtn = document.querySelector(".play");
+                playBtn.innerText = 'Play Again?';
+                playBtn.addEventListener("click", () => {
+                    window.location.reload();
+                })
+            }
+
+            if (boardCounter === 9 && hasWon != true) {
+                console.log('draw');
+                winner.innerText = 'DRAW';
+            }
+
+        })
+    }
 })();
 
 function createPlayer(name, mark) {
-    let score = 0;
-    return { name, mark, score };
+    return { name, mark };
 }
 
-function checkWinAndDraw(mark) {
+function checkWin(mark) {
     // Check rows and columns
     for (let i = 0; i < 3; i++) {
-        if (gameBoard.arr[i][0] &&
-            gameBoard.arr[i][1] &&
+        if (gameBoard.arr[i][0] === mark &&
+            gameBoard.arr[i][1] === mark &&
             gameBoard.arr[i][2] === mark) {
                 return 'Win';
             }
-        else if (gameBoard.arr[0][i] &&
-                 gameBoard.arr[1][i] &&
+        else if (gameBoard.arr[0][i] === mark &&
+                 gameBoard.arr[1][i] === mark &&
                  gameBoard.arr[2][i] === mark) {
                     return 'Win';
                  }
     } 
 
     // Check diagonal
-    if (gameBoard.arr[0][0] &&
-        gameBoard.arr[1][1] &&
+    if (gameBoard.arr[0][0] === mark &&
+        gameBoard.arr[1][1] === mark &&
         gameBoard.arr[2][2] === mark) {
             return 'Win';
         }
-    else if (gameBoard.arr[0][2] &&
-             gameBoard.arr[1][1] &&
+    else if (gameBoard.arr[0][2] === mark &&
+             gameBoard.arr[1][1] === mark &&
              gameBoard.arr[2][0] === mark) {
                 return 'Win';
             }
-    
-    // If none of the above then draw
-    return 'Draw';
 }
 
